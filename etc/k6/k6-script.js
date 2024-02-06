9c53af5fdee961a7697c56f5d1552ffc3875cc05
Miguel Galindo Rodriguez <mgalindo@palo-it.com>
2024-02-06 11:53:18 -0600

    Added k6 and influx db

--- etc/k6/k6-script.js
+++ etc/k6/k6-script.js
@@ -1,13 +1,21 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+export const options = {
+  stages: [
+    { duration: '1m', target: 1 },
+    { duration: '1m', target: 2 },
+    { duration: '1m', target: 10 },
+    { duration: '1m', target: 20 },
+  ],
+};
+
 export default function () {
-  var server_list = ["localhost:8080", "localhost:8081", "localhost:8082"]
+  var server_list = ["app-a:8080", "app-b:8080", "app-c:8080"]
   var endpoint_list = ["/", "/io_task", "/cpu_task", "/random_sleep", "/random_status", "/chain", "/error_test"]
   server_list.forEach(function(server) {
     endpoint_list.forEach(function(endpoint) {
       http.get("http://" + server + endpoint);
     });
   });
-  sleep(0.5);
 }
